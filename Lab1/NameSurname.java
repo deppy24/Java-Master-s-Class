1016a5035d5cc63851250e359b376a1645aa8f94
Deppy <sketeridespoina@gmail.com>
2025-03-27 20:40:34 +0200

    Add Java examples and remove empty Lab 1 file

---
+++ Lab1/NameSurname.java
@@ -0,0 +1,7 @@
+//Να κατασκευάσετε πρόγραμμα που εκτυπώνει το όνομά σας και στην από κάτω γραμμή το επώνυμό σας.
+public class NameSurname {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Despoina\nSurname: Sketeri");
+    }
+}
---
+++ Lab1/Rectangular.java
@@ -0,0 +1,17 @@
+//Να σχεδιάσετε ένα παραλληλόγραμμο χρησιμοποιώντας τους χαρακτήρες:
+//"-" για τις οριζόντιες γραμμές, "1" για τις κάθετες γραμμές, "+" για τις γωνίες.
+//Να εμφανίσετε το ονοματεπώνυμό σας, μέσα σε αυτό το παραλληλόγραμμο
+public class Rectangular {
+    //Δημιουργία του παραλληλογράμμου me τον πιο απλο τροπο
+    public static void main(String[] args) {
+        System.out.println("+++++++++++++");
+        System.out.println("+           +");
+        System.out.println("+           +");
+        System.out.println("+  Sketeri  +");
+        System.out.println("+           +");
+        System.out.println("+           +");
+        System.out.println("+           +");
+        System.out.println("+           +");
+        System.out.println("+++++++++++++");
+    }
+}
